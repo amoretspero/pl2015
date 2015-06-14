412d3adc6e58cbcb25917d77e3a4f8f9e90e0500
amoretspero <amoretspero@gmail.com>
2015-06-15 01:24:11 +0900

    Assignment12 - Prob01 to Prob13 done.

--- sf/Assignment12_01.v
+++ sf/Assignment12_01.v
@@ -15,7 +15,38 @@ Example typing_nonexample_3 :
              (tapp (tvar X) (tvar X))) \in
           T).
 Proof.
-  exact FILL_IN_HERE.
+    unfold not.
+    intros.
+    inversion H.
+    inversion H0.
+    inversion H1.
+    subst.
+    inversion H7.
+    subst.
+    inversion H5.
+    subst.
+    inversion H8.
+    subst.
+    rewrite H4 in H6.
+    inversion H6.
+    induction T1.
+    inversion H3.
+    apply IHT1_1.
+    rewrite H10 in *.
+    apply H8.
+    rewrite H9 in H8.
+    apply H8.
+    rewrite H9 in H4.
+    apply H4.
+    rewrite H9 in H6.
+    apply H6.
+    rewrite <- H10 in H9.
+    apply H9.
+    inversion H3.
+    inversion H3.
+    inversion H3.
+    inversion H3.
+    inversion H3.
 Qed.
 
 (*-- Check --*)
